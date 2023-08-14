b28f5c11c221f3f7b0f0c7b2d07afdb6cc01ce36
Dalton-Simonson <113248699+Dalton-Simonson@users.noreply.github.com>
2023-08-14 14:10:57 -0400

    directory stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/heartBeats-Front",
+  base: "heartBeats-Front/dist",
   plugins: [react()],
   test: {
     globals: true,
