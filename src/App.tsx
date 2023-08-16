613afd9d9412739187a247b3e80b0cef6d1496cf
Dalton-Simonson <113248699+Dalton-Simonson@users.noreply.github.com>
2023-08-16 13:52:59 -0400

    swapping back to browser router I hope it works

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {AboutPage} from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import {LogoutPage} from "./pages/LogoutPage";
 import {NavComponent} from "./components/NavComponent";
-import {HashRouter as Router, Route, Link, HashRouter} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, HashRouter} from "react-router-dom";
 import {Routes} from "react-router";
 import {ReviewPage} from "./pages/ReviewPage";
 
@@ -26,7 +26,7 @@ function App(this: any) {
     // render our page!
     return (
         <div className="main">
-            <HashRouter>
+            <Router>
                 <Navbar fixed="top" className="header">
                     <Navbar.Brand>
                         {/*<Header />*/}
@@ -68,7 +68,7 @@ function App(this: any) {
                         setLoggedIn={setLoggedIn}
                     />}/>
                 </Routes>
-            </HashRouter>
+            </Router>
         </div>
     );
 }
